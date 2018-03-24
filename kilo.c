ae928fd55d383b1ca52cf2b89f257ab9c797702c
Bobby Towers <sdfwer124@gmail.com>
2018-03-23 18:25:55 -0700

    fixed main function

--- kilo.c
+++ kilo.c
@@ -64,14 +64,7 @@ void editorProcessKeypress() {
 int main() {
   enableRawMode();
   while (1) {
-    char c = '\0';
-    if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) die("read");
-    if (iscntrl(c)) {
-      printf("%d\r\n", c);
-    } else {
-      printf("%d ('%c')\r\n", c, c);
-    }
-    if (c == CTRL_KEY('q')) break;
+    editorProcessKeypress();
   }
   return 0;
 }
