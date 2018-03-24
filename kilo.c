73c1f813be99625776e8c3ced84fcac2a223dbba
Bobby Towers <sdfwer124@gmail.com>
2018-03-23 18:17:52 -0700

    fixed the sections

--- kilo.c
+++ kilo.c
@@ -1,11 +1,22 @@
+/*** includes ***/
+
 #include <ctype.h>
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <termios.h>
 #include <unistd.h>
+
+/*** data ***/
+
 struct termios orig_termios;
-void die(const char *s) { … }
+
+/*** terminal ***/
+
+void die(const char *s) {
+  perror(s);
+  exit(1);
+}
 void disableRawMode() {
   if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1)
     die("tcsetattr");
@@ -20,8 +31,12 @@ void enableRawMode() {
   raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
   raw.c_cc[VMIN] = 0;
   raw.c_cc[VTIME] = 1;
+  
   if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) die("tcsetattr");
 }
+
+/*** init ***/
+
 int main() {
   enableRawMode();
   while (1) {
