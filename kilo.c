8dd15e9cf79d3d16f1f819121af4626536af186a
Bobby Towers <sdfwer124@gmail.com>
2018-03-23 18:03:26 -0700

    raw i/o

--- kilo.c
+++ kilo.c
@@ -1,7 +1,71 @@
-#include <unistd.h>
-
-int main() {
+/*** includes ***/
+/*** defines ***/
+#define KILO_VERSION "0.0.1"
+#define CTRL_KEY(k) ((k) & 0x1f)
+enum editorKey {
+  ARROW_LEFT = 1000,
+  ARROW_RIGHT,
+  ARROW_UP,
+  ARROW_DOWN,
+  DEL_KEY,
+  HOME_KEY,
+  END_KEY,
+  PAGE_UP,
+  PAGE_DOWN
+};
+/*** data ***/
+/*** terminal ***/
+void die(const char *s) { … }
+void disableRawMode() { … }
+void enableRawMode() { … }
+int editorReadKey() {
+  int nread;
   char c;
-  while (read(STDIN_FILENO, &c, 1) == 1);
-  return 0;
+  while ((nread = read(STDIN_FILENO, &c, 1)) != 1) {
+    if (nread == -1 && errno != EAGAIN) die("read");
+  }
+  if (c == '\x1b') {
+    char seq[3];
+    if (read(STDIN_FILENO, &seq[0], 1) != 1) return '\x1b';
+    if (read(STDIN_FILENO, &seq[1], 1) != 1) return '\x1b';
+    if (seq[0] == '[') {
+      if (seq[1] >= '0' && seq[1] <= '9') {
+        if (read(STDIN_FILENO, &seq[2], 1) != 1) return '\x1b';
+        if (seq[2] == '~') {
+          switch (seq[1]) {
+            case '1': return HOME_KEY;
+            case '3': return DEL_KEY;
+            case '4': return END_KEY;
+            case '5': return PAGE_UP;
+            case '6': return PAGE_DOWN;
+            case '7': return HOME_KEY;
+            case '8': return END_KEY;
+          }
+        }
+      } else {
+        switch (seq[1]) {
+          case 'A': return ARROW_UP;
+          case 'B': return ARROW_DOWN;
+          case 'C': return ARROW_RIGHT;
+          case 'D': return ARROW_LEFT;
+          case 'H': return HOME_KEY;
+          case 'F': return END_KEY;
+        }
+      }
+    } else if (seq[0] == 'O') {
+      switch (seq[1]) {
+        case 'H': return HOME_KEY;
+        case 'F': return END_KEY;
+      }
+    }
+    return '\x1b';
+  } else {
+    return c;
+  }
 }
+int getCursorPosition(int *rows, int *cols) { … }
+int getWindowSize(int *rows, int *cols) { … }
+/*** append buffer ***/
+/*** output ***/
+/*** input ***/
+/*** init ***/
